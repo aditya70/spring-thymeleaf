b6874ec35a988b53d6192f8b5d7c2593f7d546c9
Aditya <agoyal61@asu.edu>
2023-01-20 22:30:58 -0700

    first commit

---
+++ src/main/java/my/project/thymeleaf/config/MvcConfig.java
@@ -0,0 +1,9 @@
+package my.project.thymeleaf.config;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("my.project.simpleusers.web")
+public class MvcConfig {
+
+}
---
+++ src/main/java/my/project/thymeleaf/config/SimpleUsersConfig.java
@@ -0,0 +1,16 @@
+package my.project.thymeleaf.config;
+import org.springframework.context.annotation.Configuration;
+
+import edu.asu.diging.simpleusers.core.config.SimpleUsers;
+import edu.asu.diging.simpleusers.core.config.SimpleUsersConfiguration;
+
+
+
+@Configuration
+public class SimpleUsersConfig implements SimpleUsersConfiguration {
+
+    @Override
+    public void configure(SimpleUsers simpleUsers) {
+        
+    }
+}
